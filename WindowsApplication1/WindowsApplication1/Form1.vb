eeb154fb9bbc6319e59786ba127f75de617ad233
Krzysiek <neonic@wp.pl>
2015-04-22 20:19:22 +0200

    5th commit

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        MsgBox("Test message 1")
+        MsgBox("Test message 2")
     End Sub
 End Class
